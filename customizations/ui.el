ffb4996547000abd9876a9a7445fc8bf3f4fc63d
Leonardo Rodrigues <lmrodrigues@inf.ufrgs.br>
2016-07-07 22:21:51 -0300

    UI cust. changed

--- customizations/ui.el
+++ customizations/ui.el
@@ -1,5 +1,45 @@
+;;; ui.el --- Emacs User Interface Customizations
+
+;; Filename: ui.el
+;; Description: Emacs User Interface Setup
+;; Author: Leonardo Marques Rodrigues <lmrodrigues@riseup.net>
+;; Maintainer: Leonardo Marques Rodrigues <lmrodrigues@riseup.net>
+;; Copyright (C) 2016, Leonardo Marques Rodrigues, all rights reserved.
+;; Created:
+;; Version: 0.1
+;; Last-Updated:
+;;           By: Leonardo Marques Rodrigues
+;; URL: https://github.com/leormarqs/EmacsConfig
+;; Keywords:
+;; Compatibility: GNU Emacs 24.5.1
+;;
+;; Features that might be required by this library:
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published
+;; by the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU Affero General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+;;
+
+;;; Code:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; User interface customizations
 
 ;; Turn off start message
 (setq inhibit-startup-message t)
@@ -35,7 +75,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Setup how Emacs interacts with OS.
-(setq
+(defvar
  ;; Commands C-w and C-y to use the clipboard selection.
  x-select-enable-clipboard t
  ;; Commands C-w and C-y to use the primary selection.
@@ -48,3 +88,5 @@
  mouse-yank-at-point t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(provide 'ui)
+;;; ui.el ends here
